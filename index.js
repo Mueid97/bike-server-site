1f500e4ff1949cb0a735e397831e37e04f8ea6cd
Mueid <mrmuyed579@gmail.com>
2022-05-03 00:20:30 +0600

    single item load

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-const { use } = require('express/lib/router');
-require('dotenv').config();
+
+
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.h9thk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h9thk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
   try{
     await client.connect();
     const productCollection = client.db('bikersdb').collection('product');
+
     app.get('/product', async(req,res)=>{
         const query = {};
         const cursor = productCollection.find(query);
         const products = await cursor.toArray();
         res.send(products);
+    });
+
+    app.get('/product/:id', async(req,res)=>{
+        const id= req.params.id;
+        const query = {_id: ObjectId(id)};
+        const product = await productCollection.findOne(query);
+        res.send(product);
     })
 
   }finally{
